e58b518161efe70226581941e503d26a029a9786
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 11:27:50 -0800

    Add div to house splash screen elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default () => (
   <Layout>
-    <h1>Elly Maimon - Software and Web Developer</h1>
-    <FontAwesomeIcon icon={['far', 'envelope']} size="2x" />
-    <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
-    <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
-    <p>
-      Test body text some text here is the quick brown fox jumped over the lazy
-      dog. LATO lato.
-    </p>
+    <div>
+      <h1>Elly Maimon</h1>
+      <h2>Software Developer</h2>
+      <FontAwesomeIcon icon={['far', 'envelope']} size="2x" />
+      <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
+      <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+      <p>
+        Test body text some text here is the quick brown fox jumped over the
+        lazy dog. LATO lato.
+      </p>
+    </div>
   </Layout>
 )

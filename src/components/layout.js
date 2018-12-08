9c1bc9b93d4b68c478ae81fc1826c7dd40406114
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 10:57:32 -0800

    Add font awesome library to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,10 @@
 import React from "react"
 import './layout.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faEnvelope, faGithubSquare, faLinkedin)
 
 export default ({ children }) => (
   <div>

6dd62624cd3ccddd4e0a7fa1a6fa871e0aa3eeb7
ellymaimon <eliranmaimon@gmail.com>
2018-12-07 21:15:23 -0800

    Import the layout and house content of index in it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 export default () => (
-  <div>
+  <Layout>
     <h1>Elly Maimon - Developer COPSE copse</h1>
     <p>
       Test body text some text here is the quick brown fox jumped over the lazy
       dog. LATO lato.
     </p>
-  </div>
+  </Layout>
 )

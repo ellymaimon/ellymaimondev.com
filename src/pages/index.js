dbfc6ae59bb7e1926b72047d719da36716600806
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 12:29:57 -0800

    Add styling to social media container

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,30 @@ import React from 'react'
 import Layout from '../components/layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const container = {
+  maxWidth: '750px',
+  width: '100%',
+  margin: '100px auto 0 auto',
+  textAlign: 'center',
+}
+
+const socialMedia = { 
+  width: '50%',
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-evenly'
+}
+
 export default () => (
   <Layout>
-    <div
-      style={{
-        maxWidth: '750px',
-        width: '100%',
-        margin: '100px auto 0 auto',
-        textAlign: 'center',
-      }}
-    >
+    <div style={container}>
       <h1>Elly Maimon</h1>
       <h3>Software + Web Developer / Graphic Designer</h3>
-      <div>
-        <FontAwesomeIcon icon="envelope-square" size="2x" />
-        <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
-        <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+      <div style={socialMedia}>
+        <FontAwesomeIcon icon="envelope-square" size="3x" />
+        <FontAwesomeIcon icon={['fab', 'github-square']} size="3x" />
+        <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x" />
       </div>
-      <p>
-        Test body text some text here is the quick brown fox jumped over the
-        lazy dog. LATO lato.
-      </p>
     </div>
   </Layout>
 )

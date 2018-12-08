fd13911957d5d55167524beda44f6e80aec36d4e
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 12:22:05 -0800

    Add styles to container div, change envelope icon to square envelope

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default () => (
   <Layout>
-    <div>
+    <div
+      style={{
+        maxWidth: '750px',
+        width: '100%',
+        margin: '100px auto 0 auto',
+        textAlign: 'center',
+      }}
+    >
       <h1>Elly Maimon</h1>
-      <h2>Software Developer</h2>
-      <FontAwesomeIcon icon={['far', 'envelope']} size="2x" />
-      <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
-      <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+      <h3>Software + Web Developer / Graphic Designer</h3>
+      <div>
+        <FontAwesomeIcon icon="envelope-square" size="2x" />
+        <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
+        <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+      </div>
       <p>
         Test body text some text here is the quick brown fox jumped over the
         lazy dog. LATO lato.

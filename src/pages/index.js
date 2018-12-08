8cb6d365f75e86a4ac9752dd8fbf7fb71f644a6f
ellymaimon <eliranmaimon@gmail.com>
2018-12-07 20:41:03 -0800

    Add basic content to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Elly Maimon - Developer</div>
+export default () => (
+  <div>
+    <h1>Elly Maimon - Developer COPSE copse</h1>
+    <p>
+      Test body text some text here is the quick brown fox jumped over the lazy
+      dog. LATO lato.
+    </p>
+  </div>
+)

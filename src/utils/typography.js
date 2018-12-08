d53e1ca1fd2111454db1b060bf0123195b614ee6
ellymaimon <eliranmaimon@gmail.com>
2018-12-07 20:41:31 -0800

    Add typography configuration object

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,21 @@
 import Typography from 'typography'
 
-const typography = new Typography()
+const typography = new Typography({
+  title: 'Royale with Cheese',
+  scaleRatio: 2,
+  googleFonts: [
+    {
+      name: 'Copse',
+      styles: ['400'],
+    },
+    {
+      name: 'Lato',
+      styles: ['400'],
+    },
+  ],
+  headerFontFamily: ['Copse', 'serif'],
+  bodyFontFamily: ['Lato', 'sans-serif'],
+})
 
 export default typography
 export const rhythm = typography.rhythm

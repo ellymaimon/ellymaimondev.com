20da78229f86677b4085e94da674c0afca4ced22
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 11:00:27 -0800

    Add font awesome icons to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default () => (
   <Layout>
-    <h1>Elly Maimon - Developer COPSE copse</h1>
+    <h1>Elly Maimon - Software and Web Developer</h1>
+    <FontAwesomeIcon icon={['far', 'envelope']} size="2x" />
+    <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
+    <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
     <p>
       Test body text some text here is the quick brown fox jumped over the lazy
       dog. LATO lato.

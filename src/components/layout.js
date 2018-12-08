83386fc27222e74cf60824c28dc825a9cd86c90b
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 13:28:22 -0800

    Add styling to layout container div using emotion css-in-js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './layout.css'
+import { css } from 'emotion'
+import { rhythm } from '../utils/typography'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -7,5 +9,12 @@ import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 library.add(faEnvelopeSquare, faGithubSquare, faLinkedin)
 
 export default ({ children }) => (
-  <div style={{ margin: '50px', color: '#e0e2db' }}>{children}</div>
+  <div
+    className={css`
+      margin: ${rhythm(2)};
+      color: #e0e2db;
+    `}
+  >
+    {children}
+  </div>
 )

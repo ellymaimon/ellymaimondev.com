6d9644bee5c259f3e6407da336a342eeb0d01ea4
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 17:13:19 -0800

    Add 'to' props for each link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+// const navStyles = css`
+
+// `
+
 export default () => (
   <div>
-    <Link>Home</Link>
-    <Link>About</Link>
-    <Link>Projects</Link>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    <Link to='/projects'>Projects</Link>
   </div>
 )

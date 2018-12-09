1baa094eeb3a440cbe95aa7b52ec56ba42fba385
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 22:00:05 -0800

    Add styles, conditional rendering, and active styles to nav links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,54 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { css } from 'emotion'
 
-// const navStyles = css`
+const navStyles = css`
+  font-family: 'Copse';
+  font-size: 24px;
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-evenly;
+`
 
-// `
+const navLink = css`
+  transition: 0.5s;
+  transition-timing-function: ease;
 
-export default () => (
-  <div>
-    <Link to='/'>Home</Link>
-    <Link to='/about'>About</Link>
-    <Link to='/projects'>Projects</Link>
+  :hover {
+    color: #70a9a1;
+    transform: scale(1.1, 1.1);
+  }
+`
+
+const activeNavLink = css`
+  color: #70a9a1;
+  transform: scale(1.1, 1.1);
+
+  :hover {
+    cursor: normal;
+  }
+`
+
+export default ({ showHome }) => {
+  return (
+  <div className={navStyles}>
+    {showHome === 'true' && (
+      <Link className={navLink} to="/">
+        HOME
+      </Link>
+    )}
+    <Link activeClassName={activeNavLink} className={navLink} to="/about">
+      ABOUT
+    </Link>
+    <Link activeClassName={activeNavLink} className={navLink} to="/projects">
+      PROJECTS
+    </Link>
+    <Link activeClassName={activeNavLink} className={navLink} to="/skills">
+      SKILLS
+    </Link>
   </div>
 )
+    }
+
+// margin-bottom: ${rhythm(1.5)};

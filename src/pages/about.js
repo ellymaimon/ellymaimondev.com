a251795bd80c6bb6e51bf0d171fbc4351937e79a
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 22:02:09 -0800

    Add self portrait with styles to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Navigation from '../components/navigation'
+import { css } from 'emotion'
+import { rhythm } from '../utils/typography'
+import me from '../images/me_square.jpg'
+
+const imgDiv = css`
+  width: 100%;
+  max-width: 300px;
+  margin: ${rhythm(2)} auto;
+`
+
+const imgStyle = css`
+  border-radius: 100%;
+  width: 300px;
+  border: 5px solid white;
+`
 
 export default () => (
   <Layout>
-    <Navigation />
-    <h1>About</h1>
+    <Navigation showHome="true" />
+    <div className={imgDiv}>
+      <img className={imgStyle} src={me} alt="Elly enjoying a hamburger." />
+    </div>
   </Layout>
 )

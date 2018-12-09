11a13690847734c6da5ee8fe3c57b348bf67744d
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 21:59:19 -0800

    Add navigation to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Navigation from '../components/navigation'
 
 export default () => (
   <Layout>
-    <h1>Projects</h1>
+    <Navigation showHome='true'/>
   </Layout>
 )

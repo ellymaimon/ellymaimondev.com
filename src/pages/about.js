8b53caa3604776b2b00090296ff722db5edd8ca6
ellymaimon <eliranmaimon@gmail.com>
2018-12-08 17:11:44 -0800

    Add navigation component to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Navigation from '../components/navigation'
 
 export default () => (
   <Layout>
+    <Navigation />
     <h1>About</h1>
   </Layout>
 )

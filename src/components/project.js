4249fffa850d4f2f8c7f7ce5d2f83381849d4acf
ellymaimon <eliranmaimon@gmail.com>
2018-12-10 13:24:30 -0800

    Add linear gradient to overlay, add link styles, add content to overlay

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { css } from 'emotion'
 
-export default ({ imageUrl, projectTitle }) => {
+export default ({
+  imageUrl,
+  projectTitle,
+  liveSiteLink,
+  liveSiteLinkText,
+  description,
+  technologies,
+}) => {
   const imageStyle = css`
     display: block;
     height: auto;
@@ -13,7 +20,8 @@ export default ({ imageUrl, projectTitle }) => {
     bottom: 100%;
     left: 0;
     right: 0;
-    background-color: #008cba;
+    background: linear-gradient(45deg, #ffcb47, #70a9a1);
+    opacity: 0.95;
     overflow: hidden;
     width: 400px;
     height: 0;
@@ -23,15 +31,17 @@ export default ({ imageUrl, projectTitle }) => {
     position: relative;
     margin: 0 auto;
     width: 400px;
+    
     :hover .${overlay} {
       bottom: 0;
       height: 100%;
     }
   `
 
-  const text = css`
+  const textContainer = css`
+    width: 100%;
     color: white;
-    font-size: 20px;
+    font-weight: bold;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -41,13 +51,41 @@ export default ({ imageUrl, projectTitle }) => {
     text-align: center;
   `
 
+  const link = css`
+    transition: 0.5s;
+    transition-timing-function: ease;
+    color: black;
+
+    :hover {
+      color: white;
+      transform: scale(1.1, 1.1);
+    }
+  `
+
   return (
     <div>
-      <h3>{projectTitle}</h3>
+      <h3 className={css`margin-bottom: 0.5em;`}>{projectTitle}</h3>
       <div className={projectContainer}>
         <img className={imageStyle} src={imageUrl} alt={projectTitle} />
         <div className={overlay}>
-          <div className={text}>Hello World</div>
+          <div className={textContainer}>
+            <p>{description}</p>
+            <p
+              className={css`
+                word-spacing: 0.5em;
+              `}
+            >
+              {technologies}
+            </p>
+            <a
+              className={link}
+              href={liveSiteLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {liveSiteLinkText}
+            </a>
+          </div>
         </div>
       </div>
     </div>

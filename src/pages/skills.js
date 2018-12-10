4238b6efa261cece1989077e34ddd0f720ab7f72
ellymaimon <eliranmaimon@gmail.com>
2018-12-10 15:17:49 -0800

    Add skill spans with styling

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { css } from 'emotion'
 import Layout from '../components/layout'
 import Navigation from '../components/navigation'
 
+const spanStyle = css`
+  display: inline-block;
+  margin: 1em auto;
+  width: 100%;
+  font-size: 24px;
+`
+
 export default () => (
   <Layout>
-    <Navigation showHome='true'/>
+    <Navigation showHome="true" />
+    <div
+      className={css`
+        max-width: 800px;
+        margin: 2.5em auto 0 auto;
+        text-align: center;
+      `}
+    >
+      <span className={spanStyle}>HTML / CSS / JavaScript(ES6) / C# (.NET)</span>
+      <span className={spanStyle}>React / Redux / Angular / Vue</span>
+      <span className={spanStyle}>Node.js / Express / GraphQL / MySQL</span>
+      <span className={spanStyle}>Firebase / Firestore / Google APIs</span>
+      <span className={spanStyle}>Photoshop / Illustrator / InDesign</span>
+      <span className={spanStyle}>Semantic UI / Material UI</span>
+    </div>
   </Layout>
 )

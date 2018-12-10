d68be55217df3df8c7b868a9ba108b3351fdc326
ellymaimon <eliranmaimon@gmail.com>
2018-12-09 22:34:31 -0800

    Move styles to inside of export, change styles, refactor jsx

--- src/components/project.js
+++ src/components/project.js
@@ -2,38 +2,36 @@ import React from 'react'
 import { css } from 'emotion'
 import { rhythm } from '../utils/typography'
 
-const projectContainer = css`
-  text-align: center;
-  padding: ${rhythm(1)};
-`
+export default ({ imageUrl, projectTitle }) => {
+  const projectContainer = css`
+    text-align: center;
+    padding: ${rhythm(1)};
+  `
 
-const imageContainer = css`
-  height: 50vh;
-  max-height: 300px;
-  width: 50vw;
-  max-width: 400px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  margin: 0 auto;
-  box-shadow: 0px 0px 10px 2px #ffcb47;
-`
+  const imageContainer = css`
+    height: 300px;
+    width: 100%;
+    margin: 0 auto;
+    box-shadow: 0px 0px 10px 2px #ffcb47;
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${imageUrl});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+  `
 
-const headingInImage = css`
-  color: #191716;
-  position: relative;
-  top: 20%;
-  font-size: 36px;
-  font-weight: bold;
-`
+  const headingInImage = css`
+    color: #191716;
+    position: relative;
+    top: 20%;
+    font-size: 36px;
+    font-weight: bold;
+  `
 
-export default ({ imageUrl, projectTitle }) => (
-  <div className={projectContainer}>
-    <div
-      className={imageContainer}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      <h3 className={headingInImage}>{projectTitle}</h3>
+  return (
+    <div className={projectContainer}>
+      <div className={imageContainer}>
+        <h3 className={headingInImage}>{projectTitle}</h3>
+      </div>
     </div>
-  </div>
-)
+  )
+}
